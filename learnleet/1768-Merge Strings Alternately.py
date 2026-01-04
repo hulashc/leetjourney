6a5e9cb717db4eb70081c70cd92bdb3d601efb4d
Hulash  chand <40060031+hulashc@users.noreply.github.com>
2026-01-04 22:51:38 +0000

    yeah

--- learnleet/1768-Merge Strings Alternately.py
+++ learnleet/1768-Merge Strings Alternately.py
@@ -1,12 +1,12 @@
-def mergeAlternately(word1, word2):
+def mergeAlternately(w1, w2):
     result = []
     i = 0
 
-    while i < len(word1) or i < len(word2):
-        if i < len(word1):
-            result.append(word1[i])
-        if i < len(word2):
-            result.append(word2[i])
+    while i < len(w1) or i < len(w2):
+        if i < len(w1):
+            result.append(w1[i])
+        if i < len(w2):
+            result.append(w2[i])
         i += 1
 
     return "".join(result)
